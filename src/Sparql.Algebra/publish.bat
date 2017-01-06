be5d3f553ef9d3175937346cd3428a8fad783c92
Temitope O. Ajileye <temitope.a@live.it>
2017-01-06 04:41:24 +0000

    version 0.1 final

--- src/Sparql.Algebra/publish.bat
+++ src/Sparql.Algebra/publish.bat
@@ -1,5 +1,5 @@
 @echo off
 if "%1" == "Release" (
-dotnet pack --version-suffix "build%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%" --output C:\LibraryPackages --no-build --configuration %1
+dotnet pack --version-suffix "build%date:~6,4%%date:~3,2%%date:~0,2%%time:~1,1%%time:~3,2%%time:~6,2%" --output C:\LibraryPackages --no-build --configuration %1
 robocopy bin\\Release C:\\LibraryPackages\\%2 /E /MIR
 )
